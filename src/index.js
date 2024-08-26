9f4191d1393bc65045cc94ce2844094d9c7be021
nas233 <nas233@gmail.com>
2024-08-27 00:51:16 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let el of str) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let open = bracketsConfig[i][0];
+      let close = bracketsConfig[i][1];
+
+      if (el === open) {
+        // Special case: if the same character is used for both open and close
+        if (open === close) {
+          if (stack.length > 0 && stack[stack.length - 1] === i) {
+            stack.pop();
+          } else {
+            stack.push(i);
+          }
+        } else {
+          stack.push(i);
+        }
+      } else if (el === close) {
+        if (stack.length === 0 || stack.pop() !== i) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
